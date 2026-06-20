7b987c2107bb7227e11736da797e3e98b33aed47
rajnishsingh61 <freefirebnlrajnish@gmail.com>
2026-06-20 09:53:34 +0530

    Initial: settings.gradle.kts - Module configuration

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+rootProject.name = "OpenVirtualSpace"
+include(":app")
+include(":virtualengine")
+include(":database")
+include(":uicommon")
